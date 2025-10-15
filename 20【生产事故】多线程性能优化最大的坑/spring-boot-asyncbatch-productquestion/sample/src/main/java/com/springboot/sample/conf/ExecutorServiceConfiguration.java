55781e9329524b6457613387338217a1eeae036d
dyed-bear <1839833172@qq.com>
2025-10-15 21:32:19 +0800

    2025-10-15 21:31:57【多线程: 优化设置自定义线程池配置】

--- 20【生产事故】多线程性能优化最大的坑/spring-boot-asyncbatch-productquestion/sample/src/main/java/com/springboot/sample/conf/ExecutorServiceConfiguration.java
+++ 20【生产事故】多线程性能优化最大的坑/spring-boot-asyncbatch-productquestion/sample/src/main/java/com/springboot/sample/conf/ExecutorServiceConfiguration.java
@@ -8,6 +8,15 @@ import java.util.concurrent.*;
 @Configuration
 public class ExecutorServiceConfiguration {
 
+    /**
+     * 自定义线程池配置
+     * 核心线程数：2 * CPU 核心数 + 1
+     * 最大线程数：CPU 核心数 * 5
+     * 线程空闲时间：5 分钟
+     * 队列：SynchronousQueue
+     * 拒绝策略：CallerRunsPolicy
+     * @return
+     */
     @Bean
     public ExecutorService executorService() {
 //        return new ThreadPoolExecutor(
@@ -15,7 +24,7 @@ public class ExecutorServiceConfiguration {
 //                coreSize * 5,
 //                5L,
 //                TimeUnit.MINUTES,
-//                new LinkedBlockingQueue<Runnable>(1024)
+//                new LinkedBlockingQueue<Runnable>(1024));
         int coreSize = Runtime.getRuntime().availableProcessors();
         return new ThreadPoolExecutor(
                 2 * coreSize + 1,
